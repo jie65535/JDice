52b61d41d1ebe441e1f09b8f8690e5bf17a703b9
jie65535 <jie65535@qq.com>
2023-02-02 09:57:37 +0000

    Update version to `0.1.1`

--- src/main/kotlin/JDice.kt
+++ src/main/kotlin/JDice.kt
@@ -12,7 +12,7 @@ object JDice : KotlinPlugin(
     JvmPluginDescription(
         id = "top.jie65535.dice",
         name = "J Dice",
-        version = "0.1.0",
+        version = "0.1.1",
     ) {
         author("jie65535")
     }
